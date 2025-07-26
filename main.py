807ef58f7ea63dfa80ad10f44badf9fcfe8df407
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-07-26 16:06:26 +0500

    Changes

--- main.py
+++ main.py
@@ -56,6 +56,8 @@ def load_data():
 
     df['80/160'] = pd.to_numeric(df['80/160'], errors='coerce').fillna(0).astype(int)
     df['Total Amount'] = pd.to_numeric(df['Total Amount'], errors='coerce').fillna(0).astype(int)
+    df['Rider Cash Submission to DFPL'] = pd.to_numeric(df['Rider Cash Submission to DFPL'], errors='coerce').fillna(0)
+
 
     return df, datetime.now()
 

a859883df889faa783b632b9f7fb7bda574632ba
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-07-31 13:38:43 +0500

    c

--- main.py
+++ main.py
@@ -226,7 +226,7 @@ if selected_riders or selected_invoice_type or selected_shifts:
     st.markdown(
         f"📅 **{start_date.strftime('%d-%b-%Y')} to {end_date.strftime('%d-%b-%Y')}** &nbsp;&nbsp;&nbsp;"
         f"🧍 **{len(selected_riders)} rider(s) selected** &nbsp;&nbsp;&nbsp;"
-        f"📄 **{', '.join(selected_invoice_type)} &nbsp;&nbsp;&nbsp;**"
+        f"📄 <b>{', '.join(selected_invoice_type)}</b>&nbsp;&nbsp;&nbsp;"
         f"🕑 **{', '.join(selected_shifts) if selected_shifts else 'None'}**",
         unsafe_allow_html=True
     )

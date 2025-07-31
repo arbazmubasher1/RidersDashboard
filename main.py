3769c0f6dbfdac019e4ebbeb4fcd4a8f3ed4e521
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-07-31 13:46:10 +0500

    c

--- main.py
+++ main.py
@@ -240,18 +240,17 @@ closing_status_emojis = {
     "Pending": "⏳"
 }
 
+st.markdown("<div class='card'><h3>📢 Rider Closing Status</h3>", unsafe_allow_html=True)
 
 closing_status_counts = filtered_df['Closing Status'].dropna().value_counts()
 
-st.markdown("<div class='card'><h3>🔒 Rider Closing Status</h3>", unsafe_allow_html=True)
-for status, count in closing_status_counts.items():
+for label, value in closing_status_counts.items():
     col1, col2 = st.columns([3, 1])
     with col1:
-        st.markdown(f"<div class='card-metric'>{status}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div class='card-metric'>{label}</div>", unsafe_allow_html=True)
     with col2:
-        st.markdown(f"<div class='card-metric-value'>{count}</div>", unsafe_allow_html=True)
-#t.markdown("</div>", unsafe_allow_html=True)
-
+        st.markdown(f"<div class='card-metric-value'>{value}</div>", unsafe_allow_html=True)
+st.markdown("</div>", unsafe_allow_html=True)
 
 st.markdown("---")
 # --- Grouped Metrics (Filtered) ---

f589ae694d83f5599fc01ca26fcdd7e68f224edc
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-09-09 16:41:58 +0500

    c

--- main.py
+++ main.py
@@ -464,7 +464,7 @@ basic_metrics = {
     "Completed": (filtered_df['Order Status'].str.lower() == 'completed').sum(),
     "Cancelled at Branch": (filtered_df['Order Status'].str.lower() == 'cancel order at branch').sum(),
     "Cancelled at Customer Doorstep": (filtered_df['Order Status'].str.lower() == 'cancel order at customer doorstep').sum(),
-    "Partial Orders": (filtered_df["Partial Order Return (Amount)"] > 0).sum()
+    "Partial Orders": (filtered_df["Partial Order Return (Amount)"] > 0).count()
 }
 
 st.markdown("<div class='card'><h3>📊 Basic Information</h3>", unsafe_allow_html=True)

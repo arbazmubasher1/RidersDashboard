e4a0014255d92a46fe2f24a42e282b5338b85327
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-09-10 14:18:54 +0500

    c

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ if selected_riders or selected_invoice_type or selected_shifts:
         <div class='card'>
             <h3 style="margin:0 0 8px 0;">📌 Current Filters</h3>
             <div class='card-metric'>📅 {start_date.strftime('%d-%b-%Y')} to {end_date.strftime('%d-%b-%Y')}</div>
-            <div class='card-metric'>🧍 {len(selected_riders)} rider(s) selected</div>
+            <div class='card-metric'>🧍 {selected_riders} rider(s) selected</div>
             <div class='card-metric'>📄 {', '.join(selected_invoice_type)}</div>
             <div class='card-metric'>🕑 {', '.join(selected_shifts) if selected_shifts else 'None'}</div>
         </div>
